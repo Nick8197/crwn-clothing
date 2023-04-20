79a8fcb96e498d01776ad7d238096b1f7f250bc9
Nick8197 <nickj8197@gmail.com>
2023-04-20 07:47:25 -0500

    Using the authentication with email and google sign in and working on styling

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,28 +1,27 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import './navigation.styles.scss';
-
+import "./navigation.styles.scss";
 
 const Navigation = () => {
-    return (
-      <Fragment>
-        <div className="navigation">
-        <Link className="logo-container" to='/'>
-          <CrwnLogo className='logo'/>
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
         </Link>
-          <div className="nav-links-container">
-            <Link className="nav-link" to='/shop'>
-                SHOP
-            </Link>
-            <Link className="nav-link" to='/sign-in'>
-                SIGN IN
-            </Link>
-            </div>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/auth">
+            SIGN IN
+          </Link>
         </div>
-        <Outlet />
-      </Fragment>
-    )
-  };
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
-  export default Navigation;
+export default Navigation;
